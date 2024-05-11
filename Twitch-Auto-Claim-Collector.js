8913b7127cf28875102d0e549ee5afe381828e5b
Exoriem <48604843+Exoriem@users.noreply.github.com>
2024-05-12 01:56:21 +0200

    Create Twitch-Auto-Claim-Collector.js

---
+++ Twitch-Auto-Claim-Collector.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Twitch-Auto-Claim-Collector
+// @namespace    https://github.com/Exoriem/Twitch-Auto-Claim-Collector
+// @version      2024-05-11
+// @description  A script for tampermonkey that checks every 5 seconds if there is claim reward button and collects it.
+// @author       Exoriem
+// @match        https://www.twitch.tv/*
+// @icon         https://assets.twitch.tv/assets/favicon-32-e29e246c157142c94346.png
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    setInterval(function() {
+        var elements = document.getElementsByClassName('ScCoreButton-sc-ocjdkq-0 ScCoreButtonSuccess-sc-ocjdkq-5 bTXTVH fEpwrH');
+        elements[0].click();
+    }, 5000);
+})();
